8721a3a17f2c31e649fbbcb6a6cb9033251f50a6
william.liu <sloveliu@gmail.com>
2024-08-29 22:51:31 +0800

    fix: 修正在 docker 中運行 npm run watch 無法監聽更新及 cpu 過高

--- develop/docker-entrypoint.sh
+++ develop/docker-entrypoint.sh
@@ -3,7 +3,7 @@ printf "\n\nStarting PHP 7.4 daemon...\n\n"
 php-fpm7 --daemonize
 printf "Starting Nginx...\n\n"
 set -e
-npm run watch &
+npm run watch-poll &
 if [[ "$1" == -* ]]; then
     set -- nginx -g daemon off; "$@"
 fi
