cd724149bbe3d8f002f3689b58f95f7b14196b93
william.liu <sloveliu@gmail.com>
2024-09-08 10:05:33 +0800

    fix: docker-compose 啟動 link laravel stroage 目錄

--- develop/docker-entrypoint.sh
+++ develop/docker-entrypoint.sh
@@ -3,6 +3,8 @@ printf "\n\nStarting PHP 7.4 daemon...\n\n"
 php-fpm7 --daemonize
 printf "Starting Nginx...\n\n"
 set -e
+# 把 laravel 的 storage/app/public link 到 /public 底下
+php artisan storage:link
 npm run watch-poll &
 if [[ "$1" == -* ]]; then
     set -- nginx -g daemon off; "$@"
